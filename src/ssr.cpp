c176bbf607881ddb37310558ef14c30bfda580ef
MillerMC96 <pywang@ucdavis.edu>
2019-09-05 00:03:13 -0700

    no compilation errors

--- src/ssr.cpp
+++ src/ssr.cpp
@@ -48,10 +48,22 @@ class residue
     coor coordinate;
 };
 
-std::vector<residue>& calculate_ssr(std::vector<residue>& input1, 
-                                    std::vector<residue>& input2)
+float calculate_ssr(std::vector<residue>& input1, 
+                    std::vector<residue>& input2)
 {
+    float result = 0;
+    float dx2 = 0, dy2 = 0, dz2 = 0;
     
+    /* from x0 to x7 */
+    for (int i = 0; i < 8; i++) {
+        dx2 += pow(input1[i].coordinate.x - input2[i].coordinate.x, 2);
+        dy2 += pow(input1[i].coordinate.y - input2[i].coordinate.y, 2);
+        dz2 += pow(input1[i].coordinate.z - input2[i].coordinate.z, 2);
+    }
+
+    result = dx2 + dy2 + dz2;
+    
+    return result;
 }
 
 void read_residues(std::ifstream& input_file, std::vector<residue>& res_buffer)
@@ -81,23 +93,19 @@ int main(int argc, char* argv[])
     std::ifstream coor_file1;
     std::ifstream coor_file2;
     std::ofstream ssr_file;
-    std::string line, resname;
     std::vector<residue> res_buffer1;
     std::vector<residue> res_buffer2;
-    float x, y, z;
-    int ID;
+    float ssr;
 
     coor_file1.open(argv[1]);
     coor_file2.open(argv[2]);
     ssr_file.open(argv[3]);
 
-    for (int i = 0; i < 8; i++) {
-        avg_coor(res_buffer1[i], frames);
-        mean_file << res_buffer1[i].resn << " " << res_buffer1[i].resid 
-        << " " << res_buffer1[i].coordinate.x << " " 
-        << res_buffer1[i].coordinate.y << " " << res_buffer1[i].coordinate.z 
-        << std::endl;
-    }
+    read_residues(coor_file1, res_buffer1);
+    read_residues(coor_file2, res_buffer2);
+    ssr = calculate_ssr(res_buffer1, res_buffer2);
+
+    ssr_file << "Sum of squared residuals: " << ssr << std::endl;
 
     coor_file1.close();
     coor_file2.close();
