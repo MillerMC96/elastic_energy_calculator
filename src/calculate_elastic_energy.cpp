79686fd82ab88e77e80f58617861e62562f2c0e3
MillerMC96 <pywang@ucdavis.edu>
2019-09-09 23:21:43 -0700

    added displacement correction

--- src/calculate_elastic_energy.cpp
+++ src/calculate_elastic_energy.cpp
@@ -65,6 +65,23 @@ float calculate_ssr(std::vector<residue>& input1,
     return result;
 }
 
+coor get_avg_displacement(std::vector<residue>& input, std::vector<residue>& mean_pos)
+{
+    float dx = 0, dy = 0, dz = 0;
+
+    for (int i = 0; i < 8; i++) {
+        dx += input[i].coordinate.x - mean_pos[i].coordinate.x;
+        dy += input[i].coordinate.y - mean_pos[i].coordinate.y;
+        dz += input[i].coordinate.z - mean_pos[i].coordinate.z;
+    }
+
+    dx /= 8;
+    dy /= 8;
+    dz /= 8;
+
+    return coor(dx, dy, dz);
+}
+
 void read_residues(std::ifstream& input_file, std::vector<residue>& res_buffer)
 {
     std::string line, resname;
@@ -101,6 +118,7 @@ int main(int argc, char* argv[])
     float x, y, z;
     int ID;
     int frames = 1;
+    coor displacement_offset;
 
     coor_file.open(argv[1]);
     mean_file.open(argv[2]);
@@ -117,10 +135,11 @@ int main(int argc, char* argv[])
         } else {
             /* sum up all frames of coordinates */
             if (res_buffer.size() > 0) {
-                for (int i = 0; i < 8; i++) {
-                    elastic_energy += calculate_ssr(res_buffer, mean_buffer);
+                if (frames == 1) {
+                    displacement_offset = get_avg_displacement(res_buffer, mean_buffer);
                 }
-            
+                
+                elastic_energy = calculate_ssr(res_buffer, mean_buffer);
                 energy_file << frames << " " << elastic_energy << std::endl;
                 frames++;
                 elastic_energy = 0;
